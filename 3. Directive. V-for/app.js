119fa268c07651658c437953efa6bf09e9aa8b3a
Sergiu Chilat <chilatsergiu@gmail.com>
2021-04-08 08:48:13 +0300

    Directive. V-for

---
+++ 3. Directive. V-for/app.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        students: [
+            "Lungu Andrei",
+            "Maidacenco Anastasia",
+            "Maneev Dumitru",
+            "Mihalachi Alexandrina",
+            "Munteanu Victor"
+        ]
+    }
+})
